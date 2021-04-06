3876335e92990bec79e13db33d0c77b55f2b8ce0
Swadesh Behera <itswadesh@gmail.com>
2021-04-06 17:44:21 +0530

    -

--- components/SearchAutocomplete.vue
+++ components/SearchAutocomplete.vue
@@ -1,36 +1,37 @@
 <template>
-<div class="full">
-    <div @click="onUnfocused" class="fixed inset-0 bg-gray-500"></div>
-  <div class="z-50 flex flex-col justify-center">
-    <div class="relative flex flex-wrap w-1/4">
-    <form @submit.prevent="onSubmit">
-      <input
-        @focus="onFocused()"
-        @input="getData()"
-        placeholder="enter text"
-        @keyup="onSelectValue($event)"
-        v-model="selectedVal"
-        class="w-full border border-gray-600 rounded"
-      />
-    </form>
-      <div
-        v-if="isFocused"
-        style="z-index:9999"
-        class="w-full overflow-auto border-gray-400 rounded shadow"
-      >
+  <div class="w-full pr-4 rounded lg:pr-0 lg:w-2/3 lg:ml-44">
+    <div v-if="isFocused" @click="onUnfocused" class="fixed inset-0 w-screen h-screen bg-gray-500"></div>
+    <div class="z-50 flex flex-col justify-center w-full">
+      <div class="relative flex flex-wrap rounded">
+        <form @submit.prevent="onSubmit" class="w-full">
+          <input
+            @focus="onFocused()"
+            @input="getData()"
+            placeholder="Search For Toys"
+            @keyup="onSelectValue($event)"
+            v-model="selectedVal"
+            class="w-full p-2 pl-4 font-light border-0 rounded text-normal placeholder focus:outline-none focus:ring-1 sm:focus:ring-2 focus:ring-yellow-600"
+          />
+        </form>
         <div
-          :class="{ 'bg-gray-300': selectedIndex == i }"
-          v-for="(v, i) in products"
-          :key="i"
-          @click="onselect(v)"
-          class="flex items-center w-full text-black border-b border-gray-600 cursor-pointer hover:bg-gray-300"
+          v-if="isFocused"
+          style="z-index: 9999"
+          class="w-full mt-1 overflow-auto bg-white border-gray-400 rounded shadow"
         >
-          {{ v._source.name }}
+          <div
+            :class="{ 'bg-gray-300': selectedIndex == i }"
+            v-for="(v, i) in products"
+            :key="i"
+            @click="onselect(v)"
+            class="flex items-center w-full text-sm font-thin text-gray-400 border-b cursor-pointer hover:bg-gray-100"
+          >
+           <img v-lazy="v._source.img" :key="v._id" alt="" class="object-contain w-12 h-10 mx-2" />
+            <p class="p-3 "> {{ v._source.name }} </p>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -40,69 +41,78 @@ export default {
       products: null,
       selectedVal: null,
       isFocused: false,
-      product:null,
+      product: null,
       selectedIndex: -1,
-    };
+    }
   },
   methods: {
-      onSubmit(){
-        this.$router.push(`${this.product._source.slug}?id=${this.product._id}`)  
-      },
-     async onselect(val){
-          // console.log(val)
-          await this.onUnfocused()
-          this.product=val
-          this.selectedVal=this.product._source.name
-          this.$router.push(`/${this.product._source.slug}?id=${this.product._id}`)
-          // console.log(this.selectedVal)
-      },
+    onSubmit() {
+      this.onUnfocused()
+      if(this.product)
+      this.$router.push(`/${this.product._source.slug}?id=${this.product._id}`)
+      else
+      this.$router.push(this.selectedVal)
+    },
+    async onselect(val) {
+      // console.log(val)
+      this.fillValue(val)
+      this.onUnfocused()
+    
+      this.$router.push(`/${this.product._source.slug}?id=${this.product._id}`)
+      // console.log(this.selectedVal)
+    },
+    async fillValue(val){
+  this.product = val
+      this.selectedVal = this.product._source.name
+    },
     async onSelectValue(e) {
       // console.log(e);
       if (this.isFocused) {
-        if (e.key == "ArrowDown") {
+        if (e.key == 'ArrowDown') {
           if (this.selectedIndex < this.products.length) {
-            this.selectedIndex++;
-            this.onselect(this.products[this.selectedIndex])
+            this.selectedIndex++
+            this.fillValue(this.products[this.selectedIndex])
           } else {
-            this.selectedIndex = 0;
+            this.selectedIndex = 0
           }
-        } else if (e.key == "ArrowUp") {
+        } else if (e.key == 'ArrowUp') {
           if (this.selectedIndex >= 0) {
-            this.selectedIndex--;
-            this.onselect(this.products[this.selectedIndex])
-
+            this.selectedIndex--
+            this.fillValue(this.products[this.selectedIndex])
           }
-        } else if (e.key == "Escape") {
-          this.isFocused = false;
-          this.selectedIndex = -1;
+        } else if (e.key == 'Escape') {
+          this.isFocused = false
+          this.selectedIndex = -1
         } else {
-       
+          this.selectedIndex=-1
+          // this.selectedVal=null
+          this.product=null
         }
       }
     },
-    async getData(){
-           try {
-            const result = await this.$axios.$get(
-              "/api/products/autocomplete",
-              {
-                params: { q: this.selectedVal }
-              }
-            );
-            this.products = result.data;
-          } catch (e) {
-          } finally {
-          }
+    async getData() {
+      if(this.selectedVal == null || this.selectedVal == ''  ) return
+      try {
+        const result = await this.$axios.$get('/api/products/autocomplete', {
+          params: { q: this.selectedVal },
+        })
+        console.log(result)
+        this.products = result.data
+      } catch (e) {
+        console.log(e)
+      } finally {
+      }
     },
     onFocused() {
-      this.isFocused = true;
+      this.isFocused = true
       this.getData()
     },
     onUnfocused() {
-      this.isFocused = false;
-      this.selectedIndex = -1;
-      console.log(this.isFocused,this.selectedIndex)
+      this.isFocused = false
+      this.selectedIndex = -1
+      console.log(this.isFocused, this.selectedIndex)
       return
-    }
-  }
-};
+    },
+  },
+}
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,15 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        blank-nuxt
-      </h1>
-      <h2 class="subtitle">
-        My well-made Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<div >
+  <SearchAutocomplete />
+</div>
 </template>
 
 <script>
+  import SearchAutocomplete from "../components/SearchAutocomplete";
+
 export default {
-  components: {
-  }
-}
+ components:{
+   SearchAutocomplete
+ }
+};
 </script>
