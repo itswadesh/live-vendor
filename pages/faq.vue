f1a4524e046be7ddafefa62f2f1d33105242a60c
Swadesh Behera <itswadesh@gmail.com>
2021-04-07 19:10:27 +0530

    faq page

---
+++ pages/faq.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-primary-500">
+    FAQ
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,7 @@ module.exports = {
         }
       },
       fontFamily: {
-        sans: ["sofia-pro"]
+        sans: ["Inter"]
       }
     }
   },
