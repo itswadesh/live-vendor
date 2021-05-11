1ebf5b8f2e83a9f3da256cca902aa25671f99655
Swadesh Behera <itswadesh@gmail.com>
2021-05-11 21:51:23 +0530

    gql and apollo backend

---
+++ components/Livenow/Sidebar.vue
@@ -0,0 +1,109 @@
+<template>
+  <section class="p-4 text-gray-800 w-72">
+    <!-- Recommended channels section start  -->
+    <div class="flex items-center justify-between">
+      <h4 class="font-semibold uppercase">Recommemded Channels</h4>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-5 h-5"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </div>
+    <!-- Recommended channels section end -->
+    <!-- Channels section start  -->
+    <div class="py-2">
+      <div v-for="(i, ix) in items" :key="ix" class="flex justify-between pt-2">
+        <div class="flex items-center">
+          <div class="w-10 h-10 rounded-full">
+            <img
+              :src="i.img"
+              alt=""
+              class="object-cover object-top w-10 h-10 rounded-full"
+            />
+          </div>
+          <div class="ml-2">
+            <h5 class="font-semibold uppercase">{{ i.name1 }}</h5>
+            <h5 class="text-sm uppercase">{{ i.name2 }}</h5>
+          </div>
+        </div>
+        <div class="flex items-center">
+          <div class="w-3 h-3 mr-1 bg-red-500 rounded-full"></div>
+          <span>{{ i.live }}</span>
+        </div>
+      </div>
+    </div>
+    <!-- Channels section end -->
+    <!-- Sign up section start  -->
+    <div class="p-4 my-5 bg-white rounded-md shadow-md">
+      <h1 class="text-xl font-bold tracking-wide uppercase">
+        join the
+        <span class="text-indigo-500 cursor-pointer">twitch</span> community!!!
+      </h1>
+      <h5 class="mt-1 text-gray-600">
+        Discover the best live streams anywhere
+      </h5>
+      <button
+        class="px-4 py-2 mt-2 font-bold tracking-wide text-white bg-indigo-500 rounded-md shadow-md hover:bg-indigo-400 focus:outline-none"
+      >
+        Sign up
+      </button>
+    </div>
+    <!-- Sign up section end -->
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          name1: 'valorant',
+          name2: 'valorant',
+          live: '19.7k',
+          img: '/img/workout.jpg',
+        },
+        {
+          name1: 'valorant',
+          name2: 'valorant',
+          live: '19.7k',
+          img: '/img/workout.jpg',
+        },
+        {
+          name1: 'valorant',
+          name2: 'valorant',
+          live: '19.7k',
+          img: '/img/workout.jpg',
+        },
+        {
+          name1: 'valorant',
+          name2: 'valorant',
+          live: '19.7k',
+          img: '/img/workout.jpg',
+        },
+        {
+          name1: 'valorant',
+          name2: 'valorant',
+          live: '19.7k',
+          img: '/img/workout.jpg',
+        },
+        {
+          name1: 'valorant',
+          name2: 'valorant',
+          live: '19.7k',
+          img: '/img/workout.jpg',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,89 @@
-const HTTP_ENDPOINT = "https://tapi.litekart.in";
+import {
+  HTTP_ENDPOINT,
+  ONESIGNAL_APP_ID,
+  WWW_URL,
+  head,
+  dev,
+  tailwindcss,
+  PORT,
+  GOOGLE_ANALYTICS_ID
+} from './shared/config/index'
+const whitelistPatterns = [/(slick-+|swal2-)/]
 export default {
   head: {
-    title: process.env.npm_package_name || "",
+    title: process.env.npm_package_name || '',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-  loading: { color: "#fff" },
+  loading: { color: '#fff' },
   css: [],
   ssr: false,
   components: true,
-  plugins: [{ src: "~/plugins/directives.js", ssr: false }],
-  buildModules: ["@nuxtjs/axios", "@nuxtjs/tailwindcss", "nuxt-webfontloader"],
-  modules: ["@nuxtjs/pwa", "@nuxtjs/dotenv"],
+  plugins: [{ src: '~/plugins/directives.js', ssr: false }],
+  buildModules: [
+    '@nuxtjs/apollo',
+    '@nuxtjs/tailwindcss',
+    'nuxt-purgecss',
+    'nuxt-webfontloader',
+    '@nuxtjs/google-analytics',
+    '@nuxtjs/pwa',
+    'vue-social-sharing/nuxt'
+    // 'nuxt-vite',
+  ],
+  modules: [
+    '@nuxtjs/robots',
+    '@nuxtjs/toast',
+    '@nuxtjs/proxy',
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv',
+    'cookie-universal-nuxt',
+    'vue-sweetalert2/nuxt'
+  ],
+  pageTransition: 'slide-bottom',
+  layoutTransition: 'slide-bottom',
+  toast: {
+    singleton: true
+  },
+  purgeCSS: {
+    // whitelist,
+    whitelistPatterns
+  },
   axios: {
     proxy: true
   },
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: `${WWW_URL}/graphql`,
+        browserHttpEndpoint: '/graphql'
+        // wsEndpoint: server.replace('http', 'ws') + '/graphql',
+      }
+    },
+    defaultOptions: {
+      $query: {
+        loadingKey: 'loading',
+        fetchPolicy: 'cache-and-network'
+      }
+    }
+  },
   proxy: {
-    "/graphql": HTTP_ENDPOINT,
-    "/api": HTTP_ENDPOINT
+    '/graphql': HTTP_ENDPOINT,
+    '/api': HTTP_ENDPOINT
   },
   webfontloader: {
     google: {
-      families: ["Inter:400,700&display=swap"]
+      families: ['Inter:400,700&display=swap']
     }
   },
   build: {
     extend(config, ctx) {}
   }
-};
+}
